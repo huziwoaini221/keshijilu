f0870df20e07131ec9f86f7ab4d348550909f731
torweb321 <torweb321@gmail.com>
2026-07-30 07:59:36 +0800

    add self-destroying SW to force cleanup

---
+++ public/sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', () => {
+  self.registration.unregister()
+  caches.keys().then(keys => keys.forEach(key => caches.delete(key)))
+  self.clients.claim()
+})
